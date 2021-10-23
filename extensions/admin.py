ea754df2652f51bd408228f7bf4951aec13d9cc3
RickVeltrop <75987001+RickVeltrop@users.noreply.github.com>
2021-10-23 14:04:20 +0200

    Showwarns
    - showwarns command done
    - Aliases done

--- extensions/admin.py
+++ extensions/admin.py
@@ -2,13 +2,13 @@ import json
 import discord
 from discord.ext import commands
 
-JsonFile = 'json/data.json'
+JsonFile = 'json/warns.json'
 
 class admin(commands.Cog, name='Admin commands'):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(aliases=['g'])
+    @commands.command(aliases=['g', 'perms'])
     async def getperms(self, ctx):
         # Check if a user was mentioned or default to author #
         user = ctx.message.mentions[0] if len(ctx.message.mentions) > 0 else ctx.author
@@ -23,7 +23,9 @@ class admin(commands.Cog, name='Admin commands'):
     async def warn(self, ctx, reason):
         # Check if a user was mentioned #
         user = ctx.message.mentions[0] if len(ctx.message.mentions) > 0 else None
-        if user is None: await ctx.send(embed=discord.Embed(title="Request failed!", description="You need to mention a user to warn."))
+        if user is None:
+            await ctx.send(embed=discord.Embed(title="Request failed!", description="You need to mention a user to warn."))
+            return
 
         # Read current data from file #
         with open(JsonFile, 'r') as file:
@@ -42,15 +44,38 @@ class admin(commands.Cog, name='Admin commands'):
             file.close()
             data.clear()
 
-    @commands.command(aliases=['s'])
+    @commands.command(aliases=['s', 'warns'])
     async def showwarns(self, ctx):
+        # Check if a user was mentioned #
+        user = ctx.message.mentions[0] if len(ctx.message.mentions) > 0 else None
+        if user is None:
+            await ctx.send(embed=discord.Embed(title="Request failed!", description="You need to mention a user to show."))
+            return
+
         # Read current data from file #
         with open(JsonFile, 'r') as file:
             data = dict(json.load(file))
             file.close()
 
-        print(data)
-        print()
+        # Prepare discord embed #
+        Embed = discord.Embed(title='Warns', description='Warns for <user>, first item is for format')
+        Embed.add_field(name='Admin ID', value='Reason', inline=False)
+
+        # Bool to determine whether there is data #
+        HasData = False
+        for userid, val in data.items():
+            # Check if this item is the user we're looking for #
+            if str(user.id) in userid:
+                for item in data[userid]['warns']:
+                    # If data has been found #
+                    HasData = True
+                    Embed.add_field(name=item[0], value=item[1], inline=False)
+
+        # Report back to user #
+        if HasData:
+            await ctx.send(embed=Embed)
+        elif not HasData:
+            await ctx.send(embed=discord.Embed(title='No data!', description='No data was found for this user.'))
 
 
 def setup(bot):
