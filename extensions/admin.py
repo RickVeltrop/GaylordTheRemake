0fbd4ecebbb1087a90ddd9f603b33ce1f988e025
RickVeltrop <75987001+RickVeltrop@users.noreply.github.com>
2021-10-29 21:28:51 +0200

    Embed colors
    Added random embed colors

--- extensions/admin.py
+++ extensions/admin.py
@@ -3,8 +3,7 @@ import discord
 import datetime
 from attributes import adminatt as a
 from discord.ext import commands
-from includes import randomcolor as randcol
-
+import includes
 TimeFormat = '%H:%M:%S (gmt-1) on %a %d/%m/%y'
 JsonFile = 'json/warns.json'
 LockedChannels = []
@@ -27,7 +26,7 @@ class admin(commands.Cog, name='Admin commands'):
         # Get users perms and create embed #
         UserPerms = dict(user.guild_permissions)
         EmbedText = f',\n'.join(f'{key}: {val}' for key, val in UserPerms.items())
-        Embed = discord.Embed(title=f'Permissions for {user.name}', description=EmbedText, color=randcol())
+        Embed = discord.Embed(title=f'Permissions for {user.name}', description=EmbedText, color=includes.randomcolor())
         await ctx.send(embed=Embed)
 
     @commands.command(aliases=a.lockchannel['aliases'], brief=a.lockchannel['brief'], description=a.lockchannel['description'], enabled=a.lockchannel['enabled'], hidden=a.lockchannel['hidden'], usage=a.lockchannel['usage'])
@@ -41,7 +40,7 @@ class admin(commands.Cog, name='Admin commands'):
 
         # Edit channel perms and report to user #
         await channel.set_permissions(ctx.guild.default_role, send_messages=False)
-        await ctx.send(embed=discord.Embed(title='Success!', description=f'Locked {channel.name}.', color=randcol()))
+        await ctx.send(embed=discord.Embed(title='Success!', description=f'Locked {channel.name}.', color=includes.randomcolor()))
 
     @commands.command(aliases=a.unlock['aliases'], brief=a.unlock['brief'], description=a.unlock['description'], enabled=a.unlock['enabled'], hidden=a.unlock['hidden'], usage=a.unlock['usage'])
     @commands.has_permissions(manage_channels=True)
@@ -51,12 +50,12 @@ class admin(commands.Cog, name='Admin commands'):
 
         # Check if given channel is locked #
         if channel not in LockedChannels:
-            await ctx.send(embed=discord.Embed(title='Request failed!', description='This channel is not locked', color=randcol()))
+            await ctx.send(embed=discord.Embed(title='Request failed!', description='This channel is not locked', color=includes.randomcolor()))
             return
 
         # Edit channel perms and report to user #
         await channel.set_permissions(ctx.guild.default_role, send_messages=True)
-        await ctx.send(embed=discord.Embed(title='Success!', description=f'Unlocked {channel.name}.', color=randcol()))
+        await ctx.send(embed=discord.Embed(title='Success!', description=f'Unlocked {channel.name}.', color=includes.randomcolor()))
 
     @commands.command(aliases=a.kick['aliases'], brief=a.kick['brief'], description=a.kick['description'], enabled=a.kick['enabled'], hidden=a.kick['hidden'], usage=a.kick['usage'])
     @commands.has_permissions(kick_members=True)
@@ -64,17 +63,17 @@ class admin(commands.Cog, name='Admin commands'):
         # Check if a user was mentioned #
         user = ctx.message.mentions[0] if len(ctx.message.mentions) > 0 else None
         if user is None:
-            await ctx.send(embed=discord.Embed(title="Request failed!", description="You need to mention a user to kick.", color=randcol()))
+            await ctx.send(embed=discord.Embed(title="Request failed!", description="You need to mention a user to kick.", color=includes.randomcolor()))
             return
 
         # Check whether reason is within allowed length #
         if len(kickreason) > 50:
-            await ctx.send(embed=discord.Embed(title="Request failed!", description="Your reason has to be shorter.", color=randcol()))
+            await ctx.send(embed=discord.Embed(title="Request failed!", description="Your reason has to be shorter.", color=includes.randomcolor()))
             return
 
         # Kick member and report back to the user #
         await ctx.guild.kick(user=user, reason=kickreason)
-        await ctx.send(embed=discord.Embed(title="Success!", description=f"{user.name} was kicked from the server", color=randcol()))
+        await ctx.send(embed=discord.Embed(title="Success!", description=f"{user.name} was kicked from the server"))
 
     @commands.command(aliases=a.ban['aliases'], brief=a.ban['brief'], description=a.ban['description'], enabled=a.ban['enabled'], hidden=a.ban['hidden'], usage=a.ban['usage'])
     @commands.has_permissions(ban_members=True)
@@ -82,17 +81,17 @@ class admin(commands.Cog, name='Admin commands'):
         # Check if a user was mentioned #
         user = ctx.message.mentions[0] if len(ctx.message.mentions) > 0 else None
         if user is None:
-            await ctx.send(embed=discord.Embed(title="Request failed!", description="You need to mention a user to ban.", color=randcol()))
+            await ctx.send(embed=discord.Embed(title="Request failed!", description="You need to mention a user to ban.", color=includes.randomcolor()))
             return
 
         # Check whether reason is within allowed length #
         if len(banreason) > 50:
-            await ctx.send(embed=discord.Embed(title="Request failed!", description="Your reason has to be shorter.", color=randcol()))
+            await ctx.send(embed=discord.Embed(title="Request failed!", description="Your reason has to be shorter.", color=includes.randomcolor()))
             return
 
         # Ban member and report back to the user #
         await ctx.guild.ban(user=user, reason=banreason, delete_message_days=0)
-        await ctx.send(embed=discord.Embed(title="Success!", description=f"{user.name} was banned from the server", color=randcol()))
+        await ctx.send(embed=discord.Embed(title="Success!", description=f"{user.name} was banned from the server", color=includes.randomcolor()))
 
     @commands.command(aliases=a.warn['aliases'], brief=a.warn['brief'], description=a.warn['description'], enabled=a.warn['enabled'], hidden=a.warn['hidden'], usage=a.warn['usage'])
     @commands.has_permissions(kick_members=True)
@@ -100,12 +99,12 @@ class admin(commands.Cog, name='Admin commands'):
         # Check if a user was mentioned #
         user = ctx.message.mentions[0] if len(ctx.message.mentions) > 0 else None
         if user is None:
-            await ctx.send(embed=discord.Embed(title="Request failed!", description="You need to mention a user to warn.", color=randcol()))
+            await ctx.send(embed=discord.Embed(title="Request failed!", description="You need to mention a user to warn.", color=includes.randomcolor()))
             return
 
         # Check whether reason is within allowed length #
         if len(reason) > 50:
-            await ctx.send(embed=discord.Embed(title="Request failed!", description="Your reason has to be shorter.", color=randcol()))
+            await ctx.send(embed=discord.Embed(title="Request failed!", description="Your reason has to be shorter.", color=includes.randomcolor()))
             return
 
         # Read current data from file #
@@ -131,7 +130,7 @@ class admin(commands.Cog, name='Admin commands'):
             data.clear()
 
         # Report back to the user #
-        await ctx.send(embed=discord.Embed(title='Success!', description=f'Warned {user.name} for {reason}', color=randcol()))
+        await ctx.send(embed=discord.Embed(title='Success!', description=f'Warned {user.name} for {reason}', color=includes.randomcolor()))
 
     @commands.command(aliases=a.showwarns['aliases'], brief=a.showwarns['brief'], description=a.showwarns['description'], enabled=a.showwarns['enabled'], hidden=a.showwarns['hidden'], usage=a.showwarns['usage'])
     @commands.has_permissions(kick_members=True)
@@ -139,7 +138,7 @@ class admin(commands.Cog, name='Admin commands'):
         # Check if a user was mentioned #
         user = ctx.message.mentions[0] if len(ctx.message.mentions) > 0 else None
         if user is None:
-            await ctx.send(embed=discord.Embed(title="Request failed!", description="You need to mention a user to show.", color=randcol()))
+            await ctx.send(embed=discord.Embed(title="Request failed!", description="You need to mention a user to show.", color=includes.randomcolor()))
             return
 
         # Read current data from file #
@@ -148,7 +147,7 @@ class admin(commands.Cog, name='Admin commands'):
             file.close()
 
         # Prepare discord embed #
-        Embed = discord.Embed(title=f'Warns for {user.name}:', color=randcol)
+        Embed = discord.Embed(title=f'Warns for {user.name}:', color=includes.randomcolor())
 
         # Bool to determine whether there is data #
         HasData = False
@@ -167,7 +166,7 @@ class admin(commands.Cog, name='Admin commands'):
         if HasData:
             await ctx.send(embed=Embed)
         elif not HasData:
-            await ctx.send(embed=discord.Embed(title='No data!', description='No data was found for this user.', color=randcol()))
+            await ctx.send(embed=discord.Embed(title='No data!', description='No data was found for this user.', color=includes.randomcolor()))
 
 
 def setup(bot):
