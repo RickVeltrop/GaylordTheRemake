6072bc17305fde024f2f9918f5eef54d78740f36
RickVeltrop <75987001+RickVeltrop@users.noreply.github.com>
2021-11-26 21:07:01 +0100

    More logging things
    basic log storage

--- extensions/logging.py
+++ extensions/logging.py
@@ -1,8 +1,10 @@
 import datetime
 import discord
+import json
 from discord.ext import commands
 
 Format = f'%H:%M:%S (-2) %a %d/%m/%y'
+JsonFile = 'json/logs.json'
 
 class log():
     def __init__(self, command, admin):
@@ -12,19 +14,28 @@ class log():
         self.admin = admin
         self.time = CurrTime
 
-class admin(commands.Cog, name='Logging commands'):
+class logging(commands.Cog, name='Logging commands'):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
     @commands.has_permissions()
-    async def createlog(self, ctx):
-        await ctx.reply('Test command for creating logs')
+    async def log(self, ctx):
+        print('Test command for creating logs')
+
+        data = {}
+        data[ctx.command.name+str(ctx.author.id)] = {'Command': ctx.command.name, 'AdminName': f'{ctx.author.name}#{ctx.author.discriminator}', 'AdminId': ctx.author.id}
+
+        # Write updated data to file and clear data #
+        with open(JsonFile, 'w') as file:
+            json.dump(data, file)
+            file.close()
+            data.clear()
 
     @commands.command()
     @commands.has_permissions()
     async def showlogs(self, ctx):
-        await ctx.reply('Test command for showing logs')
+        print('Test command for showing logs')
 
 def setup(bot):
-    bot.add_cog(admin(bot))
+    bot.add_cog(logging(bot))
